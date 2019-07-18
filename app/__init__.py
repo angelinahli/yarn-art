ee802b9ed857ab2c89ed672b25a1c17b0532180b
Angelina Li <ali6@wellesley.edu>
2019-07-18 16:02:13 -0400

    set up flask app

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+-e from flask import Flask
+
+-e app = Flask(__name__)
+
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,6 @@
+-e from app import app
+
+@app.route("/")
+@app.route("/index/")
+def index():
+    return "Hello World"!
